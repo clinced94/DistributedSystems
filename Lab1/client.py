7340d107a8797b64116fc0ccb6a3d5691e4cf6cb
pgeogheg <pgeogheg@tcd.ie>
2016-11-03 14:47:32 +0000

    Updated client.py

--- Lab1/client.py
+++ Lab1/client.py
@@ -1,10 +1,10 @@
 import socket
 import sys
 
-sock = socket.create_connection(('localhost', 8000))
+sock = socket.create_connection(('10.62.0.106', 8000))
 
 try:
-	message = "GET /echo.php?message=moo HTTP/1.0\r\nHost:localhost:8000\r\n\r\n"
+	message = "GET /echo.php?message=moo HTTP/1.0\r\nHost:10.62.0.106:8000\r\n\r\n"
 	sock.send(message)
 
 	data = sock.recv(1024)
