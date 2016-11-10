b657df52aa5856f3e36149b1b8019d1053430ff2
pgeogheg <pgeogheg@tcd.ie>
2016-11-10 13:34:43 +0000

    Added killswitch

--- Lab2/multithreaded-server/src/Main.hs
+++ Lab2/multithreaded-server/src/Main.hs
@@ -29,30 +29,30 @@ initSocket = do
 	bind newSocket $ SockAddrInet 7000 iNADDR_ANY
 	return newSocket
 
-receiveMessage :: Socket -> MVar Int -> IO ()
-receiveMessage sock count = do
-	message <- Network.Socket.recv sock 2048
+receiveMessage :: Socket -> MVar Int -> MVar () -> IO ()
+receiveMessage sock count killSwitch = do
+	message <- Network.Socket.recv sock 4096
 	System.IO.putStrLn $ "Message: " ++ message
-	handleMessage sock message count
+	handleMessage sock message count killSwitch
 
-handleMessage :: Socket -> String -> MVar Int -> IO ()
-handleMessage s msg count 	| startswith msg "HELO text" 	= Network.Socket.ByteString.send s (pack msg) >> return ()
-							| startswith msg "KILL_SERVICE"	= endThread s count
-							| otherwise = return ()
+handleMessage :: Socket -> String -> MVar Int -> MVar () -> IO ()
+handleMessage s msg count killSwitch 	| startswith msg "HELO text" 	= Network.Socket.ByteString.send s (pack msg) >> return ()
+										| startswith msg "KILL_SERVICE"	= putMVar killSwitch ()
+										| otherwise = return ()
 
 endThread :: Socket -> MVar Int -> IO ()
 endThread s count = do
 	close s
 	incSocketCount count
 
-server :: Socket -> IO ()
-server sock = do 
+server :: Socket -> MVar () -> IO ()
+server sock killSwitch = do 
 	listen sock 4
 	count <- newMVar 4
-	serverLoop sock count
+	serverLoop sock count killSwitch
 
-serverLoop :: Socket -> MVar Int -> IO ()
-serverLoop sock count = do
+serverLoop :: Socket -> MVar Int -> MVar () -> IO ()
+serverLoop sock count killSwitch = do
 	(usableSocket,clientInfo) <- accept sock
 	System.IO.putStrLn $ "Connection from: " ++ (show clientInfo)
 	currentCount <- takeMVar count
@@ -60,17 +60,19 @@ serverLoop sock count = do
 	if currentCount == 0
 		then do
 			close usableSocket
-			serverLoop sock count
+			serverLoop sock count killSwitch
 			else do
 				decSocketCount count
-				_ <- forkFinally (receiveMessage usableSocket count) (\_ -> endThread usableSocket count)
-				serverLoop sock count
+				_ <- forkFinally (receiveMessage usableSocket count killSwitch) (\_ -> endThread usableSocket count)
+				serverLoop sock count killSwitch
 
 main :: IO ()
 main = do
 	newSocket <- initSocket
 	System.IO.putStrLn "Starting server..."
-	_ <- forkIO $ server newSocket
+	killSwitch <- newEmptyMVar
+	_ <- forkIO $ server newSocket killSwitch
+	takeMVar killSwitch
 	System.IO.putStrLn "Terminating server"
 
 startswith :: String -> String -> Bool
